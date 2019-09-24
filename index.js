5fda2f59dbdc4ed3640a7cedf59475b4eb9154df
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-09-24 17:21:01 +0800

    feat: Add bookmark feature

--- index.js
+++ index.js
@@ -11,24 +11,41 @@ async function getData(pageUrl) {
   return data
 }
 
-document.addEventListener('readystatechange', () => {
+document.addEventListener('readystatechange', async () => {
   if (document.readyState === 'complete') {
     console.log('readystate is complete')
-    window.location = '/#/'
+
+    window.addEventListener(
+      'hashchange',
+      async () => {
+        console.log('hashchange', window.location.hash)
+        const url = window.location.hash.split('#')[1] + '.md'
+        const data = await getData(url)
+        const container = document.querySelector('.content-container')
+
+        container.innerHTML = marked(data, { gfm: true })
+
+        const bookmarkScrollTop = await idbKeyval.get('bookmark.scrollTop')
+        window.scrollTo(0, bookmarkScrollTop || 0)
+
+        // Clear bookmark
+        await idbKeyval.clear()
+      },
+      false
+    )
+
+    const bookmarkPage = await idbKeyval.get('bookmark.page')
+    window.location = bookmarkPage || '/#/table-of-contents'
+
     document.body.classList.add('ready')
   }
 })
 
-window.addEventListener('hashchange', async () => {
-  console.log('hashchange', window.location.hash)
-  let url
-  if (window.location.hash === '#/') {
-    url = '/table-of-contents.md'
-  } else {
-    url = window.location.hash.split('#')[1] + '.md'
+document.addEventListener('visibilitychange', async () => {
+  console.log(document.visibilityState, 'visibility change')
+  // Update app state `bookmark.page` and `bookmark.scrollTop`
+  if (document.visibilityState === 'hidden') {
+    await idbKeyval.set('bookmark.page', window.location.hash)
+    await idbKeyval.set('bookmark.scrollTop', window.scrollY)
   }
-  const data = await getData(url)
-  const container = document.querySelector('.content-container')
-
-  container.innerHTML = marked(data, { gfm: true })
-}, false)
+})
