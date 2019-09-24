207c39479c90a0b58288b489c9061bd8ea710569
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-09-24 14:22:11 +0800

    feat: Fetch md and render on hashchange

--- index.js
+++ index.js
@@ -3,3 +3,32 @@ if ('serviceWorker' in navigator) {
     console.log('Service Worker Registered')
   })
 }
+
+async function getData(pageUrl) {
+  const req = new Request(pageUrl)
+  const resp = await fetch(req)
+  const data = await resp.text()
+  return data
+}
+
+document.addEventListener('readystatechange', () => {
+  if (document.readyState === 'complete') {
+    console.log('readystate is complete')
+    window.location = '/#/'
+    document.body.classList.add('ready')
+  }
+})
+
+window.addEventListener('hashchange', async () => {
+  console.log('hashchange', window.location.hash)
+  let url
+  if (window.location.hash === '#/') {
+    url = '/table-of-contents.md'
+  } else {
+    url = window.location.hash.split('#')[1] + '.md'
+  }
+  const data = await getData(url)
+  const container = document.querySelector('.content-container')
+
+  container.innerHTML = marked(data, { gfm: true })
+}, false)
