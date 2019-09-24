a69d92c124c3ed4c4176c73a0399873412173d97
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-09-24 14:23:56 +0800

    feat: Disable cache except for cors

--- sw.js
+++ sw.js
@@ -1,17 +1,19 @@
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('video-store').then(function(cache) {
-      const chapters = Array.from(
-        new Array(28),
-        (_, i) => `/chapter-${i + 1}.html`
-      )
+    caches.open('lotus-sutra').then(function(cache) {
+      // const chapters = Array.from(
+      //   new Array(28),
+      //   (_, i) => `/chapter-${i + 1}.html`
+      // )
+      const chapters = []
       return cache.addAll(
         chapters.concat([
-          '/',
-          '/index.html',
-          '/index.js',
-          '/style.css',
-          '/icon.png'
+          // '/',
+          // '/index.html',
+          // '/index.js',
+          // '/style.css',
+          // '/icon.png',
+          'https://cdnjs.cloudflare.com/ajax/libs/marked/0.7.0/marked.min.js',
         ])
       )
     })
@@ -34,9 +36,9 @@ self.addEventListener('activate', event => {
     caches.keys().then(keyList => {
       return Promise.all(
         keyList.map(key => {
-        if (!cacheKeeplist.includes(key)) {
-          return caches.delete(key)
-        }
+          if (!cacheKeeplist.includes(key)) {
+            return caches.delete(key)
+          }
         })
       )
     })
