f28a51e82e3971641f925af66a903b4155d559ad
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-09-28 09:37:03 +0800

    feat: Add ChapterContent component

---
+++ src/components/ChapterContent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="chapter-content" v-html="content"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    chapterNum: {
+      type: [Number, String],
+      required: true,
+    },
+  },
+
+  watch: {
+    chapterNum: {
+      handler(newValue) {
+        this.content = this.getData(newValue)
+      },
+      immediate: true,
+    },
+  },
+
+  methods: {
+    getData(newValue) {
+      return `<h1>Got Content</h1><p>Chapter ${newValue} </p>`
+    },
+  },
+
+  data() {
+    return {
+      content: '',
+    }
+  },
+}
+</script>
--- src/views/Chapter.vue
+++ src/views/Chapter.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="chapter">
     <h1>This is chapter {{ $route.params.chapter_num }}</h1>
+
+    <chapter-content :chapter-num="$route.params.chapter_num" />
   </div>
 </template>
+
+<script>
+import ChapterContent from '@/components/ChapterContent'
+export default {
+  components: { ChapterContent },
+}
+</script>
