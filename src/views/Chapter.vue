56c6e30880a7af13645b91def1b9c58c9308752b
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-09-30 09:48:20 +0800

    feat: Add previous and next chapter links

--- src/views/Chapter.vue
+++ src/views/Chapter.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="chapter">
-    <router-link to="/">目录</router-link>
+    <div class="chapter__nav">
+      <router-link :to="`/chapter/${chapterNum - 1}`" v-if="chapterNum > 1"
+        >上一章</router-link
+      >
+      <router-link to="/">目录</router-link>
+      <router-link :to="`/chapter/${chapterNum + 1}`" v-if="chapterNum < 28"
+        >下一章</router-link
+      >
+    </div>
 
-    <base-chapter :chapter-num="$route.params.chapter_num" />
+    <base-chapter :chapter-num="chapterNum" />
 
-    <router-link to="/">目录</router-link>
+    <div class="chapter__nav">
+      <router-link :to="`/chapter/${chapterNum - 1}`" v-if="chapterNum > 1"
+        >上一章</router-link
+      >
+      <router-link to="/">目录</router-link>
+      <router-link :to="`/chapter/${chapterNum + 1}`" v-if="chapterNum < 28"
+        >下一章</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -13,5 +29,26 @@ import BaseChapter from '@/components/BaseChapter/BaseChapter'
 
 export default {
   components: { BaseChapter },
+
+  computed: {
+    chapterNum() {
+      return parseInt(this.$route.params.chapter_num)
+    },
+  },
 }
 </script>
+
+<style lang="scss">
+.chapter {
+  &__nav {
+    display: flex;
+    flex-wrap: wrap;
+    align-content: space-between;
+
+    a {
+      flex-grow: 1;
+      width: max-content;
+    }
+  }
+}
+</style>
