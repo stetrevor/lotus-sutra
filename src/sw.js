12d947890d595922d7045c24df4aa0e68f50e3ee
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-04 18:28:05 +0800

    feat: Remove unused sw.js

--- src/sw.js
+++
@@ -1,7 +0,0 @@
-workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
-
-self.addEventListener('message', event => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting()
-  }
-})
