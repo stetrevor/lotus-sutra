553aa376eff05f21bfd49628820187f23417561d
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-04 13:10:39 +0800

    feat: Precache in sw.js
    sw.js is a stub service worker file, the import statement is generated
    from `yarn build`.

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,5 @@
+workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
+
 self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting()
