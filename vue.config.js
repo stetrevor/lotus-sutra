2c2a85d0dd7aef89bcb2a3d47116f97ab9e611be
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-05 09:12:53 +0800

    feat: Use relative publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { GenerateSW } = require('workbox-webpack-plugin')
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/lotus-sutra/' : '/',
+  publicPath: '',
 
   chainWebpack: config => {
     config.module
