2d6936197ee90d27ad744d2fcbbdc858d1af7c7d
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-05 12:04:01 +0800

    feat: Add workbox as Vue instance property

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './registerServiceWorker'
+import workbox from './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$workbox = workbox
 
 new Vue({
   router,
